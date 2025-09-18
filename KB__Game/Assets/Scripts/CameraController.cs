63e311b2d5f79b784863b663b1dd6797fcd10e6c
R <rianipad2@gmail.com>
2025-09-18 18:32:46 +0700

    encounter

---
+++ KB__Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float zoom;
+    private float zoomMultiplier = 0.2f;
+    private float velocity = 0f;
+    private float smoothTime = 0.25f;
+    private bool startZoom = false;
+    [SerializeField] private new Camera camera;
+    void Start()
+    {
+        zoom = camera.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startZoom)
+        {
+            Debug.Log("Zooming");
+            zoom -= zoomMultiplier;
+            camera.orthographicSize = Mathf.SmoothDamp(zoom, zoom * zoomMultiplier, ref velocity, smoothTime);
+            if (zoom <= 2.5f)
+            {
+                startZoom = false;
+                GameControl.LOAD_COMBAT();
+            }
+        }
+    }
+
+    public void ENCOUNTER_ZOOM()
+    {
+        startZoom = true;
+    }
+}
--- KB__Game/Assets/Scripts/Player/Player.cs
+++ KB__Game/Assets/Scripts/Player/Player.cs
@@ -11,6 +11,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            GameControl.FREEZE();
+            playerMovement.enabled = false;
+            FindFirstObjectByType<CameraController>().ENCOUNTER_ZOOM();
+        }
     }
 }
